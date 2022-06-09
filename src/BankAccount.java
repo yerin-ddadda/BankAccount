1dec14667aa1a84e39a1e8eff82b1cad85689baa
yerin <yerin0110@naver.com>
2022-06-09 15:36:24 +0900

    접근 제어자 연습

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,19 +1,19 @@
 public class BankAccount {
-    int balance;
-    Person owner;
+    private int balance;
+    private Person owner;
 
     // 파라미터 : 입금할 액수(정수)
     // 리턴 : 성공여부(불린)
     boolean deposit(int amount) {
         // 1. write code here
-        if (amount<0 || owner.cashAmount < amount){
-            System.out.println("입금 실패입니다. 잔고: "+ balance+"원, 현금: " + owner.cashAmount+"원");
+        if (amount<0 || owner.getCashAmount() < amount){
+            System.out.println("입금 실패입니다. 잔고: "+ balance+"원, 현금: " + owner.getCashAmount() +"원");
             return false;
         }
         else{
             balance += amount;
-            owner.cashAmount -= amount;
-            System.out.println(amount+"원 입금하였습니다. 잔고: " + balance +"원, 현금: "+ owner.cashAmount +"원");
+            owner.setCashAmount(owner.getCashAmount() - amount);
+            System.out.println(amount+"원 입금하였습니다. 잔고: " + balance +"원, 현금: "+ owner.getCashAmount() +"원");
             return true;
         }
 
@@ -24,13 +24,13 @@ public class BankAccount {
     boolean withdraw(int amount) {
         // 2. write code here
         if (amount < 0 || balance < amount) {
-            System.out.println("출금 실패입니다. 잔고: "+ balance+"원, 현금: " + owner.cashAmount+"원");
+            System.out.println("출금 실패입니다. 잔고: "+ balance+"원, 현금: " + owner.getCashAmount() +"원");
             return false;
         }
         else{
-            owner.cashAmount += amount;
+            owner.setCashAmount(owner.getCashAmount() + amount);
             balance -= amount;
-            System.out.println(amount+ "원 출금하였습니다. 잔고: " + balance +"원, 현금: " + owner.cashAmount +"원");
+            System.out.println(amount+ "원 출금하였습니다. 잔고: " + balance +"원, 현금: " +  owner.getCashAmount() +"원");
             return true;
         }
 
@@ -42,4 +42,21 @@ public class BankAccount {
     // boolean transfer(Person to, int amount) {
     // (다음 과제에서 사용하므로, 우선 skip합니다.)
     // }
+
+    public void setBalance(int newBalance){
+        balance = newBalance;
+    }
+
+    public int getBalance(){
+        return balance;
+    }
+
+    public void setOwner(Person newOwner){
+        owner = newOwner;
+    }
+
+    public Person getOwner(){
+        return owner;
+    }
+
 }
--- src/BankDriver.java
+++ src/BankDriver.java
@@ -2,26 +2,25 @@ public class BankDriver {
     public static void main(String[] args) {
         // 사람 선언
         Person p1 = new Person();
-        p1.name = "김신의";
-        p1.age = 28;
-        p1.cashAmount = 30000;
-
+        p1.setName("김신의");
+        p1.setAge(28);
+        p1.setCashAmount(30000);
 
         // 은행 계좌 생성
         BankAccount a1 = new BankAccount();
-        a1.balance = 100000;
+        a1.setBalance(100000);
 
-        p1.account = a1;
-        a1.owner = p1;
+        p1.setAccount(a1);
+        a1.setOwner(p1);
 
         // 3 - 4. write code here
         Person p2 = new Person();
         BankAccount a2 = new BankAccount();
-        a2.owner = p2;
-        p2.account = a1;
+        a2.setOwner(p2);
+        p2.setAccount(a1);
 
-        p2.cashAmount = 100000;
-        a2.balance = 500000;
+        p2.setCashAmount(100000);
+        a2.setBalance(500000);
 
         System.out.println(a2.deposit(30000));
         System.out.println(a2.withdraw(170000));
@@ -29,4 +28,3 @@ public class BankDriver {
         System.out.println(a2.withdraw(890000));
     }
 }
-
